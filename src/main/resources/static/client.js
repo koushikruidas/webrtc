e96148dcb2f6ca919390cb87ca3b7bc5fa1f6203
Koushik Ruidas <koushikruidas@gmail.com>
2023-07-05 01:34:49 +0530

    p2p transfer and showing in index page

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -64,6 +64,18 @@ function initialize() {
     // when we receive a message from the other peer, printing it on the console
     dataChannel.onmessage = function(event) {
         console.log("message:", event.data);
+        var receivedMessage = JSON.parse(event.data);
+
+          // Check the message type
+          if (receivedMessage.type === 'file') {
+            var fileData = receivedMessage.data;
+
+            // Handle the received file data
+            receiveFileData(fileData);
+          } else {
+                console.log("again printing the data in else: ",event.data)
+          }
+
     };
 
     dataChannel.onclose = function() {
@@ -132,19 +144,79 @@ function sendFile() {
 
   for (var i = 0; i < files.length; i++) {
     var file = files[i];
-    // Do something with the file, such as sending it over the data channel
-    // For example, you can use the FileReader API to read the file contents
     var reader = new FileReader();
 
     reader.onload = function (event) {
       var fileData = event.target.result;
-      // Send the file data over the data channel
-      dataChannel.send(fileData);
+      var message = {
+          type: 'file',
+          data: fileData
+      };
+
+      // Convert the message object to a JSON string
+      var jsonString = JSON.stringify(message);
+
+      dataChannel.send(jsonString);
     };
 
     reader.readAsDataURL(file);
   }
 
-  // Clear the file input
   fileInput.value = "";
 }
+
+function receiveFileData(fileData) {
+  var fileContainer = document.getElementById("fileContainer");
+  var fileType = getFileType(fileData);
+
+  if (fileType === "image") {
+    // Create download link
+    var downloadLink = document.createElement("a");
+    downloadLink.href = fileData;
+    downloadLink.download = "file";
+    downloadLink.textContent = "Download File";
+    // Append the url element to the fileContainer div
+    fileContainer.appendChild(downloadLink);
+    // Create an image (<img>) element
+    var imageElement = document.createElement("img");
+    imageElement.src = fileData;
+    // Append the image element to the fileContainer div
+    fileContainer.appendChild(imageElement);
+  } else if (fileType === "pdf") {
+    // Create an embedded PDF object
+    var pdfEmbedObject = document.createElement("embed");
+    pdfEmbedObject.src = fileData;
+    pdfEmbedObject.type = "application/pdf";
+    pdfEmbedObject.width = "100%";
+    pdfEmbedObject.height = "500px";
+
+    // Append the PDF object to the fileContainer div
+    fileContainer.appendChild(pdfEmbedObject);
+  } else {
+    // Unsupported file type or unable to determine file type
+    console.log("Unsupported file type or unable to determine file type");
+  }
+}
+
+function getFileType(fileData) {
+  var mimeType = fileData.substring(fileData.indexOf(":") + 1, fileData.indexOf(";"));
+  if (mimeType === "image/jpeg") {
+    fileType = "image";
+  } else if (mimeType === "application/pdf") {
+    fileType = "pdf";
+  }
+  return fileType;
+}
+
+function base64ToBlob(base64Data) {
+  var byteCharacters = atob(base64Data);
+  var byteArrays = [];
+
+  for (var i = 0; i < byteCharacters.length; i++) {
+    byteArrays.push(byteCharacters.charCodeAt(i));
+  }
+
+  var byteArray = new Uint8Array(byteArrays);
+  return new Blob([byteArray], { type: "application/octet-stream" });
+}
+
